bdf771123b3ee1b138a056bdf29c6314cdd8e478
Toby Hutchinson <tobyhutchinson@gmail.com>
2026-09-12 14:18:22 +0100

    Create glyph object and current selected glyph

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const ascii = { " ": 32, "!": 33, "\"": 34, "#": 35, "$": 36, "%": 37, "&": 38, "'": 39, "(": 40, ")": 41, "*": 42, "+": 43, ",": 44, "-": 45, ".": 46, "/": 47, "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, ":": 58, ";": 59, "<": 60, "=": 61, ">": 62, "?": 63, "@": 64, "A": 65, "B": 66, "C": 67, "D": 68, "E": 69, "F": 70, "G": 71, "H": 72, "I": 73, "J": 74, "K": 75, "L": 76, "M": 77, "N": 78, "O": 79, "P": 80, "Q": 81, "R": 82, "S": 83, "T": 84, "U": 85, "V": 86, "W": 87, "X": 88, "Y": 89, "Z": 90, "[": 91, "\\": 92, "]": 93, "^": 94, "_": 95, "`": 96, "a": 97, "b": 98, "c": 99, "d": 100, "e": 101, "f": 102, "g": 103, "h": 104, "i": 105, "j": 106, "k": 107, "l": 108, "m": 109, "n": 110, "o": 111, "p": 112, "q": 113, "r": 114, "s": 115, "t": 116, "u": 117, "v": 118, "w": 119, "x": 120, "y": 121, "z": 122, "{": 123, "|": 124, "}": 125, "~": 126 };
-
 const colEl = document.getElementById("columns");
 const rowEl = document.getElementById("rows");
 const glyphEl = document.getElementById("glyph-container");
@@ -9,7 +7,8 @@ let glyphGrid = [];
 let drawing = false;
 let erasing = false;
 
-function createGlyphGrid() {
+
+(function createGlyphGrid() {
     let colNum = Number(colEl.value);
     let rowNum = Number(rowEl.value);
 
@@ -41,7 +40,7 @@ function createGlyphGrid() {
             glyphEl.appendChild(div);
         }
     }
-}
+})();
 
 [colEl, rowEl].forEach((el) => {
     el.addEventListener("change", () => {
@@ -89,21 +88,35 @@ document.addEventListener("pointerup", () => {
     drawing = false;
 });
 
-createGlyphGrid();
-
 // Character selector
 const asciiCharsEl = document.getElementById("ascii-chars-container");
 
-function createAsciiChars() {
+let glyphs = {};
+
+function createNewGlyph() {
+    return Array.from(
+        { length: Number(rowEl.value) },
+        () => Array(Number(colEl.value)).fill(0)
+    );
+}
+
+(function createAsciiChars() {
     for (let i = 33; i <= 126; i++) {
         const char = document.createElement("div");
 
+        const character = String.fromCharCode(i);
         char.classList.add("ascii-char");
-        char.textContent = String.fromCharCode(i);
+        char.textContent = character;
         char.dataset.ascii = i;
 
         asciiCharsEl.appendChild(char);
+        glyphs[i] = createNewGlyph();
     }
-}
+})();
 
-createAsciiChars();
+let currentGlyph = 33;
+
+// Highlight the current glyph 
+let glyphDiv = document.querySelector(`[data-ascii='${currentGlyph}']`);
+glyphDiv.classList.add("selected-glyph");
+
